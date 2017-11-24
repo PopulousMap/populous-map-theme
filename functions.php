d33f90f8901b511da7d34fc4bd5615749339e266
Raquel Smith <raquel@foodbloggerpro.com>
2017-11-24 15:16:13 -0800

    Add `render_populous_map` shortcode

--- functions.php
+++ functions.php
@@ -1,1 +1,19 @@
 <?php
+
+function render_populous_map_func() {
+    return "<div id='map'></div>
+		<script>
+		mapboxgl.accessToken = 'pk.eyJ1IjoicG9wdWxvdXNtYXAiLCJhIjoiY2oxbWxjOWF1MDBhZzMzcGpreGR3OGJpbiJ9.zlYdACNoreNJ61pfd67KIg';
+		var map = new mapboxgl.Map({
+		 container: 'map',
+		 style: 'mapbox://styles/populousmap/cj821itn4069b2ro1xh44nror',
+		 center: [-126.196992, 54.588569], 
+		 zoom: 4.5
+		});
+
+		map.scrollZoom.disable();
+
+		</script>";
+}
+
+add_shortcode('render_populous_map', 'render_populous_map_func');
