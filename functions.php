3acd2a0a5f65ead948780ee0294e3d6668d251f8
Raquel Smith <raquel@foodbloggerpro.com>
2017-12-17 08:57:10 -0800

    Break map display html out into separate template

--- functions.php
+++ functions.php
@@ -1,14 +1,9 @@
 <?php
 
 function render_populous_map_func() {
-    return "<div id='map'></div>
-			<div class='map-overlay top'>
-				<div class='map-overlay-inner'>
-					<h2>Time Slider</h2>
-					<label id='year'></label>
-					<input id='slider' type='range' min='1778' max='2020' step='1' value='1700' />
-				</div>
-			</div>";
+    ob_start();
+    get_template_part('map_display');
+    return ob_get_clean(); 
 }
 
 add_shortcode('render_populous_map', 'render_populous_map_func');
---
+++ map_display.php
@@ -0,0 +1,8 @@
+<div id='map'></div>
+<div class='map-overlay top'>
+	<div class='map-overlay-inner'>
+		<h2>Time Slider</h2>
+		<label id='year'></label>
+		<input id='slider' type='range' min='1778' max='2020' step='1' value='1700' />
+	</div>
+</div>
