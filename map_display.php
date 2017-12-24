0dbb80d6c458fb10f3dcc0a1e816ee066e092aca
Raquel Smith <raquel@foodbloggerpro.com>
2017-12-24 10:18:58 -0800

    Create & style the category cloud

--- map_display.php
+++ map_display.php
@@ -1,4 +1,34 @@
 <div id='map'></div>
+<div class='categories'>
+	<ul>
+		<li>Food</li>
+		<li>Resistance</li>
+		<li>Water</li>
+		<li>Transportation</li>
+		<li>Inspiration</li>
+		<li>Art</li>
+		<li>Business</li>
+		<li>Medicine</li>
+		<li>People of Colour</li>
+		<li>Politics</li>
+		<li>Indigenous</li>
+		<li>Agriculture</li>
+		<li>Women</li>
+		<li>Disaster</li>
+		<li>Environmental</li>
+		<li>Medical</li>
+		<li>Mining</li>
+		<li>Mental Health</li>
+		<li>Forestry</li>
+		<li>Gatherings</li>
+		<li>Infrastructure</li>
+		<li>Languages</li>
+		<li>Queer</li>
+		<li>Epidemics</li>
+		<li>Activism</li>
+		<li>Acts</li>
+	</ul>
+</div>
 <div class="year-slider">
 	<input type="number" min="-20" max="40" step="1" id="input-number-low">
 	<div id="html5"></div>
