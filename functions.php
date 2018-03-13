85b6c393b1b36721c7a7a87e244a64467f9578c6
Raquel Smith <raquel@foodbloggerpro.com>
2018-03-13 08:18:05 -0700

    Add cachebuster to js file

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ add_shortcode('render_populous_map', 'render_populous_map_func');
 
 function load_render_map_script(){
 	wp_enqueue_script( 'mapbox_gl_js_script', 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.42.1/mapbox-gl.js' );
-	wp_enqueue_script( 'render_map_script', get_stylesheet_directory_uri() . '/render-map.js', array( 'jquery' ), false, true );
+	wp_enqueue_script( 'render_map_script', get_stylesheet_directory_uri() . '/render-map.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/render-map.js' ), true );
 	wp_enqueue_script( 'wNumb_script', get_stylesheet_directory_uri() . '/wNumb.js' );
 	wp_enqueue_script( 'noiuslider_script', get_stylesheet_directory_uri() . '/nouislider.min.js' );
 	wp_enqueue_style( 'nouislider_style', get_stylesheet_directory_uri() . '/nouislider.min.css' );
